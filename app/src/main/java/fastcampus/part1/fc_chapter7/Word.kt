fb3ef6e0491ba5fc8d13cba2c35c332acdf72a64
syb8200 <syb8200@naver.com>
2023-03-22 19:15:28 +0900

    Word data class 생성

---
+++ app/src/main/java/fastcampus/part1/fc_chapter7/Word.kt
@@ -0,0 +1,8 @@
+package fastcampus.part1.fc_chapter7
+
+// data holding 역할 (상속 불가능, 반드시 하나 이상의 property 갖음)
+data class Word(
+    val text : String,
+    val mean : String,
+    val type : String
+)
