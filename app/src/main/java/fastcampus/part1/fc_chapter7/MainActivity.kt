8f431e58f7f3297b79b8173fcd731a02e4f3f61e
syb8200 <syb8200@naver.com>
2023-03-22 20:04:02 +0900

    WordRecyclerView 클릭리스너 및 디바이더 설정

--- app/src/main/java/fastcampus/part1/fc_chapter7/MainActivity.kt
+++ app/src/main/java/fastcampus/part1/fc_chapter7/MainActivity.kt
@@ -1,11 +1,14 @@
 package fastcampus.part1.fc_chapter7
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import fastcampus.part1.fc_chapter7.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), WordAdapter.ItemClickListener {
     private lateinit var binding : ActivityMainBinding
     private lateinit var wordAdapter : WordAdapter
 
@@ -15,19 +18,34 @@ class MainActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
+        initRecyclerView()
+
+        binding.addButton.setOnClickListener {
+            val intent = Intent(this, AddActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    private fun initRecyclerView() {
         val dummyList = mutableListOf(
             Word("weather", "날씨", "명사"),
             Word("honey", "꿀", "명사"),
             Word("run", "실행하다", "동사")
         )
 
-        wordAdapter = WordAdapter(dummyList)
+        wordAdapter = WordAdapter(dummyList, this)
         binding.wordRecyclerView.apply {
             // 어댑터 연결
             adapter = wordAdapter
             // apply 안에 있기 때문에 this가 아닌 applicationContext 사용
             layoutManager = LinearLayoutManager(applicationContext, LinearLayoutManager.VERTICAL, false)
+            // 디바이더 (구분선)
+            val dividerItemDecoration = DividerItemDecoration(applicationContext, LinearLayoutManager.VERTICAL)
+            addItemDecoration(dividerItemDecoration)
         }
+    }
 
+    override fun onClick(word: Word) {
+        Toast.makeText(this, "${word.text}가 클릭되었습니다.", Toast.LENGTH_SHORT).show()
     }
 }
--- app/src/main/java/fastcampus/part1/fc_chapter7/WordAdapter.kt
+++ app/src/main/java/fastcampus/part1/fc_chapter7/WordAdapter.kt
@@ -7,7 +7,10 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import fastcampus.part1.fc_chapter7.databinding.ItemWordBinding
 
-class WordAdapter(private val list: MutableList<Word>) : RecyclerView.Adapter<WordAdapter.WordViewHolder>() {
+class WordAdapter(
+    private val list: MutableList<Word>,
+    private val itemClickListener: ItemClickListener? = null
+) : RecyclerView.Adapter<WordAdapter.WordViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): WordViewHolder {
         // ViewHolder를 생성 (ViewHolder는 View를 갖고 있음)
@@ -18,12 +21,9 @@ class WordAdapter(private val list: MutableList<Word>) : RecyclerView.Adapter<Wo
 
     override fun onBindViewHolder(holder: WordViewHolder, position: Int) {
         // UI와 data 연결
-        holder.binding.apply {
-            val word = list[position]
-            textTextView.text = word.text
-            meanTextView.text = word.mean
-            typeChip.text = word.type
-        }
+        val word = list[position]
+        holder.bind(word)
+        holder.itemView.setOnClickListener { itemClickListener?.onClick(word) }
     }
 
     override fun getItemCount(): Int {
@@ -31,7 +31,17 @@ class WordAdapter(private val list: MutableList<Word>) : RecyclerView.Adapter<Wo
         return list.size
     }
 
-    class WordViewHolder(val binding: ItemWordBinding) : RecyclerView.ViewHolder(binding.root) {
+    class WordViewHolder(private val binding: ItemWordBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(word: Word) {
+            binding.apply {
+                textTextView.text = word.text
+                meanTextView.text = word.mean
+                typeChip.text = word.type
+            }
+        }
+    }
 
+    interface ItemClickListener {
+        fun onClick(word: Word)
     }
 }
