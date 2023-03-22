fe91caadc950f861d6daedaa0465ea97290b2be5
syb8200 <syb8200@naver.com>
2023-03-22 19:36:03 +0900

    WordAdapter 생성 및 연결

--- app/src/main/java/fastcampus/part1/fc_chapter7/MainActivity.kt
+++ app/src/main/java/fastcampus/part1/fc_chapter7/MainActivity.kt
@@ -2,16 +2,32 @@ package fastcampus.part1.fc_chapter7
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import fastcampus.part1.fc_chapter7.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding : ActivityMainBinding
+    private lateinit var wordAdapter : WordAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        val dummyList = mutableListOf(
+            Word("weather", "날씨", "명사"),
+            Word("honey", "꿀", "명사"),
+            Word("run", "실행하다", "동사")
+        )
+
+        wordAdapter = WordAdapter(dummyList)
+        binding.wordRecyclerView.apply {
+            // 어댑터 연결
+            adapter = wordAdapter
+            // apply 안에 있기 때문에 this가 아닌 applicationContext 사용
+            layoutManager = LinearLayoutManager(applicationContext, LinearLayoutManager.VERTICAL, false)
+        }
+
     }
 }
---
+++ app/src/main/java/fastcampus/part1/fc_chapter7/WordAdapter.kt
@@ -0,0 +1,37 @@
+package fastcampus.part1.fc_chapter7
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import fastcampus.part1.fc_chapter7.databinding.ItemWordBinding
+
+class WordAdapter(private val list: MutableList<Word>) : RecyclerView.Adapter<WordAdapter.WordViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): WordViewHolder {
+        // ViewHolder를 생성 (ViewHolder는 View를 갖고 있음)
+        val inflater = parent.context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        val binding = ItemWordBinding.inflate(inflater, parent, false)
+        return WordViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: WordViewHolder, position: Int) {
+        // UI와 data 연결
+        holder.binding.apply {
+            val word = list[position]
+            textTextView.text = word.text
+            meanTextView.text = word.mean
+            typeChip.text = word.type
+        }
+    }
+
+    override fun getItemCount(): Int {
+        // 어댑터가 갖고 있는 데이터의 리스트 개수 반환
+        return list.size
+    }
+
+    class WordViewHolder(val binding: ItemWordBinding) : RecyclerView.ViewHolder(binding.root) {
+
+    }
+}
